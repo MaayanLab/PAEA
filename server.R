1dc88c4fbcb0db5063331ee3c2b21d97d7ebe674
zero323 <matthew.szymkiewicz@gmail.com>
2015-01-30 14:41:46 +0100

    Add data upload

--- server.R
+++ server.R
@@ -1,18 +1,103 @@
 library(shiny)
+library(ggvis)
+library(data.table)
 library(dplyr)
+
+options(shiny.maxRequestSize=120*1024^2) 
+
 library(nasbMicrotaskViewerHelpers)
 
-data <- nasbMicrotaskViewerHelpers::preprocess()
+data <- nasbMicrotaskViewerHelpers::preprocess('http://localhost/microtask.1.24.2015.csv')
 
 shinyServer(function(input, output, session) {
     
-    counts <- data$genes %>% group_by(gene) %>% summarise(n=n())
-    output$distPlot <- renderPlot({
-        bins <- seq(min(counts$n), max(counts$n), length.out = input$bins + 1)
-        
-        hist(
-            counts$n,
-            breaks = bins, col = 'darkgray', border = 'white'
+    values <- reactiveValues()
+    # Not required. Just to remind myself what is stored inside
+    values$chdir <- NULL
+    values$control_samples <- NULL
+    values$treatment_samples <- NULL
+    
+    
+    #' Ugly hack to be able to clear upload widget
+    #' 
+    output$datain_container <- renderUI({
+        fileInput(
+            'datain', 'Choose file to upload',
+            accept = c(
+                'text/csv', 'text/comma-separated-values',
+                'text/tab-separated-values', 'text/plain',
+                '.csv', '.tsv'
+            )
         )
+    })
+    
+ 
+    #' Handle 
+    #'
+    observe({
+        if(input$run_chdir == 0) return()
+        datain <- isolate(datain())
+        sampleclass <- factor(ifelse(colnames(datain)[-1] %in% isolate(input$sampleclass), '1', '2'))
+        values$chdir <- try(
+            GeoDE::chdirAnalysis(datain, sampleclass = sampleclass)
+        )
+    })
+    
+    
+    output$nprobes <- renderText({
+        if(!is.null(datain())) {
+            nrow(datain())
+        }
+    })
+    
+    output$ngenes <- renderText({
+        if(!is.null(datain())) {
+            nlevels(datain()[[1]])
+        }
+    })
+    
+    observe({
+        datain <- datain()
+        if(!is.null(datain)) {
+            samples_mask <- colnames(datain)[-1] %in%  input$sampleclass
+            values$control_samples <- colnames(datain)[-1][samples_mask]
+            values$treatment_samples <- colnames(datain)[-1][!samples_mask]
+        } else {
+            values$control_samples <- NULL
+            values$treatment_samples <- NULL
+        }
+    })
+
+    output$control_samples <- renderText({
+        paste(values$control_samples, collapse = ', ')
+    })
+    
+    output$treatment_samples <- renderText({
+        paste(values$treatment_samples, collapse = ', ')
+    })
+    
+    output$contents <- renderDataTable({
+        datain()
+    })
+    
+    output$sampleclass_container <- renderUI({
+        if (!is.null(datain()) && ncol(datain()) != 1 ) {
+            checkboxGroupInput(
+                'sampleclass',
+                'Choose control samples',
+                colnames(datain())[-1]
+            )
+        } 
+    })
+    
+    datain <- reactive({
+        inFile <- input$datain
+        values$chdir <- NULL
+        
+        if (is.null(inFile)) return(NULL)
+        # Not optimal but read.csv is easier to handle
+        as.data.table(read.csv(
+            inFile$datapath, sep = input$sep
+        ))
     })
 })
--- ui.R
+++ ui.R
@@ -1,20 +1,87 @@
 library(shiny)
 
-shinyUI(fluidPage(
+#' Input separator choice
+#'
+seperator_input <- radioButtons(
+    'sep', 'Separator',
+    c(Comma=',', Semicolon=';', Tab='\t'),
+)
 
-  titlePanel(''),
-
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput('bins',
-                  'Number of bins:',
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    mainPanel(
-      plotOutput('distPlot')
+#' Dataset download 
+#'
+upload_tab <- tabPanel(
+    'Upload dataset',
+    fluidRow(
+        column(12, p('')),
+        column(6, wellPanel(
+            h3('Expression data'),
+            uiOutput('datain_container'),
+            seperator_input        
+        )),
+        column(6, wellPanel(
+            h3('Control samples'),
+            uiOutput('sampleclass_container')
+        )),
+        column(12, dataTableOutput('contents'))
     )
-  )
-))
+)
+
+#' Characteristic Direction Analysis ouput tab
+#'
+chdir_tab <- tabPanel(
+    'Characteristic Direction Analysis',
+    fluidRow(
+        column(12, p(''))
+    ),
+    column(12, wellPanel(
+        tags$dl(
+            tags$dt('#genes:'),
+            tags$dd(textOutput('ngenes')),
+            tags$dt('#probes:'),
+            tags$dd(textOutput('nprobes')),
+            tags$dt('Control samples:'),
+            tags$dd(textOutput('control_samples')),
+            tags$dt('Treatment samples:'),
+            tags$dd(textOutput('treatment_samples'))
+        ),
+        actionButton(inputId = 'run_chdir', label = 'Run', icon = NULL)
+    ))
+)
+
+
+#' Principle Angle Enrichment Analysis ouput tab
+#'
+paea_tab <- tabPanel(
+    'Principle Angle Enrichment Analysis'
+)
+
+
+#' Data analysis tab
+#'
+analyze_panel <- tabPanel(
+    title = 'Analyze',
+    tabsetPanel(
+        upload_tab,
+        chdir_tab,
+        paea_tab
+    )
+)
+
+#' About tab
+#'
+about_panel <- tabPanel(
+    title = 'About',
+    fluidRow(column(12))
+)
+
+
+#' Complete UI
+#'
+#'
+shinyUI(
+    navbarPage(
+        'NASB Microtask Viewer',
+        analyze_panel,
+        about_panel
+    )
+)
